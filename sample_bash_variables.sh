f75d337a2c511a1f7421458c6a6379a55f21c078
Jarrod Reuter <jarrod.reuter@gmail.com>
2016-12-20 23:07:11 -0500

    Adding options to the config.

--- sample_bash_variables.sh
+++ sample_bash_variables.sh
@@ -3,7 +3,9 @@
 declare shell_option="ssh -p 22"
 declare login="username@your_server.example.com"
 declare local_user="username"
-declare base_path="/media/data"
-declare -a paths=("test1"
-                  "test2"
-                 )
+declare rsync_options="--bwlimit=1024 --delete-after"
+declare destination_path="/media/data/backup/"
+declare source_base_path="/media/data"
+declare -a source_paths=("test1"
+                         "test2"
+                        )
