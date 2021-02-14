3b702b237950881841d2482660c443339d06f960
Jarrod Reuter <jarrod.reuter@gmail.com>
2021-02-13 21:46:39 -0500

    Fixing email script.

--- utility_scripts/b2email.php
+++ utility_scripts/b2email.php
@@ -2,10 +2,10 @@
 <?PHP
 include 'b2_email_variables.php';
 
-    $output = get_file_contents("b2_output");
+    $output = file_get_contents("b2_output");
 
     $sub = "BIG NAS Backup - BackBlaze Backup Complete";
-    $msg = "Your backup has completed successfully.\n" + $output;
+    $msg = "Your backup has completed successfully.\n" . $output;
 
     $emails = explode ( " ", $to );
     $i = 0;
